0ad2bcd31ba4e95a9fa4960b4df3a246f8eebf9e
B3RZVS <79112396+B3RZVS@users.noreply.github.com>
2024-09-15 11:51:11 -0300

    creacion del proyeto
    se instalo MUI

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'; 
+import Prueba from './pages/Prueba';
+
+function App() {
+ 
+
+  return (
+    <Routes>
+      <Route element={<Prueba />} path="/" />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/components/componentePrueba/ComponentePrueba.tsx
@@ -0,0 +1,12 @@
+import style from "./prueba.module.css"
+
+
+// type Props = {}
+
+const ComponentePrueba = () => {
+  return (
+    <div className={style.contenedor}> soy componente 1</div>
+  )
+}
+
+export default ComponentePrueba
