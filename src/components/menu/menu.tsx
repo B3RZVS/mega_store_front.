b42c46320e58aa25a601c8641f31aa45baaed692
RocioMenardi <romenardi08@gmail.com>
2024-09-23 18:25:40 -0300

    Menu
    Estilos del manu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,6 +1,5 @@
 import styles from './menu.module.css';
 import AccordionUsage from './acordion';
-import { Accordion } from '@mui/material';
 import { useState } from 'react';
 
 const Menu = () => {
@@ -14,30 +13,30 @@ const Menu = () => {
     return (
     <div className={styles.container}>
         <header className={styles.header}>
-            <div> <h1 className={styles.title}>MegaStore</h1>
-            </div>
-            <nav className={styles.navbar}>
+        <nav className={styles.navbar}>
                 <div className={styles.menuIcon} onClick={toggleMenu}>
-                    <div className={styles.dropdownBtn}></div>
                     <span className={styles.burgerLine}></span>
                     <span className={styles.burgerLine}></span>
                     <span className={styles.burgerLine}></span>
                 </div>
                 {/* Contenido del menú que se muestra/oculta según el estado */}
-                <div className={styles.dropdownBtn}>
+                
                 {isMenuOpen && (
                     <div className={styles.dropdownContent}>
-                    <button className={styles.dropdownBtn}>Registrar</button>
-                    <button className={styles.dropdownItem}>Marca</button>
-                    <button className={styles.dropdownItem}>Sucursal</button>
-                    <button className={styles.dropdownItem}>Color</button>
-                    <button className={styles.dropdownItem}>Talle</button>
+                    <AccordionUsage />
                     </div>
+                    
                 )}
-                </div>
+                
             </nav>
+            <div> <h1 className={styles.title}>MegaStore</h1>
+            </div>
         </header>
-        <main className={styles.mainContent}> <AccordionUsage></AccordionUsage></main>
+
+        <main className={styles.mainContent}> 
+            
+        </main>
+
         <footer className={styles.footer}>
             <p>Copyright ©20245Bits+Ema</p>
         </footer> 
